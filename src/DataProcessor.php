2a803d2dc38acb95f7e81eb7e3f3f99baa185b1a
Chris Tenday <ctenday@gmail.com>
2023-06-08 00:48:44 +0100

    still working..

--- src/DataProcessor.php
+++ src/DataProcessor.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Rtgroup\DataProcessor;
 
 class DataProcessor
 {
@@ -8,20 +8,74 @@ class DataProcessor
     public function __construct(array $dataSet)
     {
         $this->dataSet=$dataSet;
+
     }
 
     /**
      * Method pour groupper les données par key.
      * @param $key
      */
-    public function groupBy($key)
+    public function groupBy($query)
     {
+        $query=$this->format($query);
         $keyData=[];
 
         for($i=0; $i<count($this->dataSet); $i++)
         {
-            //if($this->)
+            for($k=0; $k<count($this->dataSet[$i]); $k++)
+            {
+                $keys=array_keys($this->dataSet[$i][$k]);
+
+                foreach($this->dataSet[$i][$k] as $key=>$value)
+                {
+                    $value=$this->format($value);
+                    if($this->equals($query,$value))
+                    {
+                        echo $key."#".$value."\n";
+                    }
+
+
+                }
+                echo "===============\n";
+            }
+            exit();
         }
 
+    }
+
+    /**
+     * Method pour comparer 2 strings.
+     * @param $str1
+     * @param $str2
+     * @param bool $ignoreCase
+     * @return bool
+     */
+    private function equals($str1,$str2,$ignoreCase=true)
+    {
+        if($ignoreCase)
+        {
+            if(strcasecmp($str1,$str2)==0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        else
+        {
+            if(strcmp($str1,$str2)==0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    private function format($key)
+    {
+        $k=str_replace(" ","_",$key);
+
+        return $k;
     }
 }
